281b496b51d1635a5e27dbd9ee032082a54c63eb
camruggles <cruggles@purdue.edu>
2018-06-01 09:28:58 -0400

    this won't work

--- src/testGenerate.jl
+++ src/testGenerate.jl
@@ -3,6 +3,9 @@ module testGenerate
 # package code goes here
 function test()
 	println("Testing to see if a generated package can work.")
+	t = ccall((:help, "./libtemp.so"), Int32, ())
+	println("$t")
+
 end
 
 end # module
