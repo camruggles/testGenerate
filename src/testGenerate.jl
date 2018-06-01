fdd3d2d5439906631db49d9b2bdb9c0c7aa98d7b
camruggles <cruggles@purdue.edu>
2018-06-01 13:12:26 -0400

    Updating the dynamic linking issues

--- src/testGenerate.jl
+++ src/testGenerate.jl
@@ -4,10 +4,14 @@ module testGenerate
 function test()
 	println("Testing to see if a generated package can work.")
 	file = joinpath("$(Pkg.dir("testGenerate"))", "deps", "open.so")
-	ccall((:hello, file), Void, ())
+	handle = Libdl.dlopen(file)
+	
+	funcPtr = Libdl.dlsym(handle, :hello)
+	ccall(funcPtr, Void, ())
 	
 end
 
 end # module
 
-
+using testGenerate
+testGenerate.test()
