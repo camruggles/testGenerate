5c96bafe520f1648017e34603ce434e0a23a3efc
camruggles <cruggles@purdue.edu>
2018-06-01 09:40:58 -0400

    REmoving files

--- src/testGenerate.jl
+++ src/testGenerate.jl
@@ -3,9 +3,6 @@ module testGenerate
 # package code goes here
 function test()
 	println("Testing to see if a generated package can work.")
-	t = ccall((:help, "./libtemp.so"), Int32, ())
-	println("$t")
-
 end
 
 end # module
