9339136fa0453b89354cf7c8f7e41e0e3b538b47
camruggles <cruggles@purdue.edu>
2018-06-01 13:01:18 -0400

    Is it fixed?

--- src/testGenerate.jl
+++ src/testGenerate.jl
@@ -3,8 +3,8 @@ module testGenerate
 # package code goes here
 function test()
 	println("Testing to see if a generated package can work.")
-	file = Pkg.dir("testGenerate")
-	ccall((:hello, "$file/deps/open.so"), Void, ())
+	file = joinpath("$(Pkg.dir("testGenerate"))", "deps", "open.so")
+	ccall((:hello, file), Void, ())
 	
 end
 
